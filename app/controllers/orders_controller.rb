02232a00cb9e19d9042130563a03b31ae309b071
Jeff Cohen <cohen.jeff@gmail.com>
2017-07-26 18:53:52 -0500

    At the break

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,39 +1,34 @@
 class OrdersController < ApplicationController
 
-  def index
-    user = User.find_by(id: session[:user_id])
-    if user.blank?
+  before_action :require_login
+
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    if @user.blank?
       redirect_to root_url, notice: "You need to login first."
-    else
-      @orders = Order.all.order('created_at desc')
-    end
-  end
-  
-  def new
-    user = User.find_by(id: session[:user_id])
-    if user.blank?
-      redirect_to root_url, notice: "You need to login first."
-    else
-      @order = Order.new
-      @order.product = Product.find_by(id: params[:product_id])
-      render 'new'
     end
   end
 
+  def index
+    @orders = @user.orders.order('created_at desc')
+  end
+
+  def new
+    @order = Order.new
+    @order.product = Product.find_by(id: params[:product_id])
+    render 'new'
+  end
+
   def create
-    user = User.find_by(id: session[:user_id])
-    if user.blank?
-      redirect_to root_url, notice: "You need to login first."
+    @order = Order.new            # why is this an instance variable?
+    @order.product = Product.find_by(id: params[:product_id])
+    @order.user = @user
+
+    if @order.save
+      redirect_to root_url, notice: "Thanks for your order! We will ship it when we feel like it."
     else
-      @order = Order.new            # why is this an instance variable?
-      @order.product = Product.find_by(id: params[:product_id])
-      @order.user = user
-      if @order.save
-        redirect_to root_url, notice: "Thanks for your order! We will ship it when we feel like it."
-      else
-        @product = @order.product
-        render 'new'
-      end
+      @product = @order.product
+      render 'new'
     end
   end
 
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,17 +3,26 @@ class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
 
+  before_validation :cleanup_card_number
+  validate :valid_credit_card_number
+
+  def cleanup_card_number
+    card_number.gsub!(/\D/,'')
+  end
+
   def total_amount
     (product.price / 100.0).round(2)
   end
-  
+
   def valid_credit_card_number
     # TODO: implement Luhn algorithm here
     #
     # To cause this validation to stop the persistence chain,
     # simply add an error to the .errors collection that is
     # attached to this object.
-
+    if card_number.blank? || card_number.length != 16
+      errors.add(:card_number, "is invalid")
+    end
   end
 
 
